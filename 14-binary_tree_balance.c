6ccb3a06af0538a34879ead6ac19fd1af6349ed2
bigBadMatt <5707@holbertonstudents.com>
2023-01-11 12:35:52 -0800

    function that returns the ballance of the tree

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,21 @@
+#include "binary_tree.h"
+
+/**
+ *binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: a pointer to the root node of a tree
+ * Return: An int representing the ballance of the tree, 0 if the tree is null
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height - right_height);
+}
