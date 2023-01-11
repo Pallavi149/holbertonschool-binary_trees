09c4c89629d37a0bdac13cf2f9e5012c184eea72
bigBadMatt <5707@holbertonstudents.com>
2023-01-11 12:47:22 -0800

    fixed missing function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,3 +19,34 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (left_height - right_height);
 }
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node
+ * Return: tree height, 0 otherwise
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_branch;
+	int right_branch;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	left_branch = binary_tree_height(tree->left);
+	right_branch = binary_tree_height(tree->right);
+
+	if (left_branch < right_branch)
+	{
+		return (right_branch + 1);
+	}
+	else
+	{
+		return (left_branch + 1);
+	}
+}
