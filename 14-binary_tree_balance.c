0ca7508d013db71907d095bce999bc81e93e8b91
bigBadMatt <5707@holbertonstudents.com>
2023-01-11 12:57:38 -0800

    fixed bug

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,10 +34,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (0);
-	}
+
 	left_branch = binary_tree_height(tree->left);
 	right_branch = binary_tree_height(tree->right);
 
